4d530cdd79e8dadcb67a77362f160dcb13689d04
BecaVILo <70987056+BecaVILo@users.noreply.github.com>
2020-10-07 21:50:44 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -23,5 +23,5 @@ with(epComsumption, plot(DateTime,Global_active_power,
                          type = "l", 
                          xlab = " ",
                          ylab = "Global Active Power (kilowatts)"))
-dev.copy(png, file = "plot2.png")
+dev.copy(png, file = "plot2.png",width=480, height=480)
 dev.off()
