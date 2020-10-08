0666f48e69023fcbcc697e51a2aff815001bf21a
BecaVILo <70987056+BecaVILo@users.noreply.github.com>
2020-10-07 21:50:23 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -23,5 +23,5 @@ with(epComsumption, hist(Global_active_power,
                          col = "red", 
                          xlab = "Global Active Power (kilowatts)", 
                          main = "Global Active Power"))
-dev.copy(png, file = "plot1.png")
+dev.copy(png, file = "plot1.png",width=480, height=480)
 dev.off()
